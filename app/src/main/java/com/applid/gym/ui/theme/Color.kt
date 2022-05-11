5bb57338b50b7ec5bd3356c1cb2e7d485cd69c1b
Boze-noob <bozeskoko16@gmail.com>
2022-05-11 17:25:46 +0200

    theme setup

--- app/src/main/java/com/applid/gym/ui/theme/Color.kt
+++ app/src/main/java/com/applid/gym/ui/theme/Color.kt
@@ -6,3 +6,17 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val black = Color(0xFF0B0C10)
+val darkGrey = Color(0xFF1F2833)
+val whiteDarkTheme = Color(0xFFC5C6C7)
+val lightBlueDarkTheme = Color(0xFF66FCF1)
+val darkBlueDarkTheme = Color(0xFF45A29E)
+
+val whiteLightTheme = Color(0xFFC1C8E4)
+val lightBlueLightTheme = Color(0xFF84CEEB)
+val midLightBlueLightTheme = Color(0xFF5AB9EA)
+val darkBlueLightTheme = Color(0xFF5680E9)
+val purple = Color(0xFF8860D0)
+
+
--- app/src/main/java/com/applid/gym/ui/theme/Theme.kt
+++ app/src/main/java/com/applid/gym/ui/theme/Theme.kt
@@ -7,18 +7,21 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-        primary = Purple200,
-        primaryVariant = Purple700,
-        secondary = Teal200
+        primary = whiteDarkTheme,
+        primaryVariant = lightBlueDarkTheme,
+        secondary = darkBlueDarkTheme,
+        background = darkGrey,
 )
 
 private val LightColorPalette = lightColors(
-        primary = Purple500,
-        primaryVariant = Purple700,
-        secondary = Teal200
+        primary = whiteLightTheme,
+        primaryVariant = darkBlueLightTheme,
+        secondary = lightBlueLightTheme,
+        secondaryVariant = purple,
+        background = whiteLightTheme,
 
         /* Other default colors to override
-    background = Color.White,
+
     surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.Black,
