2836f9d15e3c8bcea4afcd4a498c754306383735
Boze-noob <bozeskoko16@gmail.com>
2022-06-20 11:41:49 +0200

    homeAppBar UI

--- app/src/main/java/com/applid/gym/ui/screens/home_screen/common/HomeAppBar.kt
+++ app/src/main/java/com/applid/gym/ui/screens/home_screen/common/HomeAppBar.kt
@@ -1,3 +1,4 @@
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
@@ -6,6 +7,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -18,14 +20,17 @@ fun HomeAppBar(mainTxt : String, descriptionTxt : String) {
         Row(
             modifier = Modifier.padding(horizontal = 15.dp, vertical = 20.dp)
         ) {
-            Column() {
+            Column(
+                modifier = Modifier.padding(vertical = 10.dp)
+            ) {
                 Text(text = mainTxt, style = MaterialTheme.typography.h1)
-                Text(text = descriptionTxt)
+                Text(text = descriptionTxt, style = MaterialTheme.typography.subtitle1)
             }
 
             Spacer(Modifier.weight(1f))
             Card(
-                modifier = Modifier.size(48.dp),
+                modifier = Modifier.size(55.dp),
+                border = BorderStroke(3.dp, MaterialTheme.colors.secondaryVariant),
                 shape = CircleShape,
                 elevation = 2.dp,
             ) {
